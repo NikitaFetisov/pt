e254f54df92e662f2330a13b33dbde31e94be25e
NikitaFetisov <90622790+NikitaFetisov@users.noreply.github.com>
2022-03-21 15:02:23 +0300

    Лабораторная работа 13

---
+++ LR13/Laba2/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelP = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxR = new System.Windows.Forms.ComboBox();
+            this.comboBoxN = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.labelR = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelX.Location = new System.Drawing.Point(66, 63);
+            this.labelX.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(29, 15);
+            this.labelX.TabIndex = 0;
+            this.labelX.Text = "X = ";
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelP.Location = new System.Drawing.Point(66, 155);
+            this.labelP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(27, 15);
+            this.labelP.TabIndex = 1;
+            this.labelP.Text = "p = ";
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelA.Location = new System.Drawing.Point(65, 250);
+            this.labelA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(26, 15);
+            this.labelA.TabIndex = 2;
+            this.labelA.Text = "a = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 276);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 3;
+            // 
+            // comboBoxR
+            // 
+            this.comboBoxR.FormattingEnabled = true;
+            this.comboBoxR.Location = new System.Drawing.Point(315, 56);
+            this.comboBoxR.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxR.Name = "comboBoxR";
+            this.comboBoxR.Size = new System.Drawing.Size(140, 23);
+            this.comboBoxR.TabIndex = 4;
+            this.comboBoxR.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBoxR_MouseClick);
+            // 
+            // comboBoxN
+            // 
+            this.comboBoxN.FormattingEnabled = true;
+            this.comboBoxN.Location = new System.Drawing.Point(315, 151);
+            this.comboBoxN.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxN.Name = "comboBoxN";
+            this.comboBoxN.Size = new System.Drawing.Size(140, 23);
+            this.comboBoxN.TabIndex = 5;
+            this.comboBoxN.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBoxN_MouseClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(510, 78);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 19);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Уравнение 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(510, 121);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(97, 19);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Уравнение 2 ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(292, 235);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(153, 67);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "        Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(488, 317);
+            this.textBoxZ.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxZ.Multiline = true;
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.ReadOnly = true;
+            this.textBoxZ.Size = new System.Drawing.Size(327, 40);
+            this.textBoxZ.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(452, 329);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Z = ";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(91, 59);
+            this.textBoxX.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(116, 22);
+            this.textBoxX.TabIndex = 11;
+            // 
+            // textBoxP
+            // 
+            this.textBoxP.Location = new System.Drawing.Point(91, 152);
+            this.textBoxP.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.Size = new System.Drawing.Size(116, 22);
+            this.textBoxP.TabIndex = 12;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(91, 247);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(116, 22);
+            this.textBoxA.TabIndex = 13;
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelR.Location = new System.Drawing.Point(279, 59);
+            this.labelR.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(31, 15);
+            this.labelR.TabIndex = 14;
+            this.labelR.Text = " R = ";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelN.Location = new System.Drawing.Point(284, 155);
+            this.labelN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(29, 15);
+            this.labelN.TabIndex = 15;
+            this.labelN.Text = "N = ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(510, 56);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Выбор уравнения";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Laba2.Properties.Resources.calculate2;
+            this.pictureBox1.Location = new System.Drawing.Point(299, 244);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 47);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 395);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.labelR);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxP);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBoxN);
+            this.Controls.Add(this.comboBoxR);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelP);
+            this.Controls.Add(this.labelX);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxR;
+        private System.Windows.Forms.ComboBox comboBoxN;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxP;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LR13/Laba2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double z = 0;
+            double x = Convert.ToDouble(textBoxX.Text);
+            double p = Convert.ToDouble(textBoxP.Text);
+            int n = Convert.ToInt32(comboBoxN.SelectedIndex.ToString());
+            int r = Convert.ToInt32(comboBoxR.SelectedIndex.ToString());
+            double a = Convert.ToDouble(textBoxA.Text);
+
+            if (radioButton1.Checked)
+            {
+                n = n + 2;
+                int Znamenatel = 2;
+                for (int i = 1; i < n; i++)
+                {
+                    z += ((Math.Pow(-1, i % 2))*((Math.Pow(p, i - 1) * Math.Pow(x, i)) / Znamenatel));
+                    Znamenatel = Znamenatel * (i + 2);
+                    
+                }
+            }
+            else
+            {
+                n = n + 1;
+                r = r + 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= r; j++)
+                    {
+                        z += ((Math.Pow(i, 2) + a * Math.Pow(j, 2))/(Math.Pow(i, 3) + Math.Pow(j, 3)));
+                    }
+                }
+            }
+            textBoxZ.Text = z.ToString();
+        }
+
+        private void comboBoxT_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBoxN_MouseClick(object sender, MouseEventArgs e)
+        {
+            comboBoxN.Items.Clear();
+            comboBoxN.Items.Add(1);
+            comboBoxN.Items.Add(2);
+            comboBoxN.Items.Add(3);
+            comboBoxN.Items.Add(4);
+            comboBoxN.Items.Add(5);
+            comboBoxN.Items.Add(6);
+            comboBoxN.Items.Add(7);
+        }
+        private void comboBoxR_MouseClick(object sender, MouseEventArgs e)
+        {
+            comboBoxR.Items.Clear();
+            comboBoxR.Items.Add(1);
+            comboBoxR.Items.Add(2);
+            comboBoxR.Items.Add(3);
+            comboBoxR.Items.Add(4);
+        }
+    }
+}
